48f0cb5c7644e0ba1075c5ef985708f9b7a77037
Krishna Murthi <110836223+Krish323@users.noreply.github.com>
2025-04-08 20:08:48 +0530

    Create python.py

---
+++ python.py
@@ -0,0 +1,51 @@
+- stage: SetPipelineVariablePython
+  displayName: 'Set Release Pipeline Variable via Python'
+  jobs:
+  - job: SetVariablePythonJob
+    displayName: 'Set Variable Job (Python)'
+    pool:
+      vmImage: 'windows-latest'
+    steps:
+    - task: UsePythonVersion@0
+      inputs:
+        versionSpec: '3.x'
+        addToPath: true
+
+    - script: |
+        import os
+        import requests
+        import json
+
+        auth_token = os.environ['SYSTEM_ACCESSTOKEN']
+        release_id = os.environ['RELEASE_RELEASEID']
+        variable_name = "CC"
+        variable_value = os.environ.get('PREDEPLOYGATE_SN_CHANGE_REQUEST_NUMBER')
+        collection_uri = os.environ['SYSTEM_COLLECTIONURI']
+        project = os.environ['SYSTEM_TEAMPROJECT']
+
+        print(f"Updating release variable '{variable_name}' to '{variable_value}'...")
+
+        release_url = f"{collection_uri}{project}/_apis/release/releases/{release_id}?api-version=5.0"
+
+        headers = {
+            'Authorization': f'Bearer {auth_token}',
+            'Content-Type': 'application/json'
+        }
+
+        response = requests.get(release_url, headers=headers)
+        release = response.json()
+
+        if variable_name not in release['variables']:
+            release['variables'][variable_name] = { "value": variable_value }
+        else:
+            release['variables'][variable_name]['value'] = variable_value
+
+        put_response = requests.put(release_url, headers=headers, data=json.dumps(release))
+        print(f"Variable '{variable_name}' updated. Status: {put_response.status_code}")
+      displayName: 'Update Release Variable Using Python'
+      env:
+        SYSTEM_ACCESSTOKEN: $(System.AccessToken)
+        RELEASE_RELEASEID: $(release.ReleaseId)
+        PREDEPLOYGATE_SN_CHANGE_REQUEST_NUMBER: $(PREDEPLOYGATE.SN.CHANGE_REQUEST_NUMBER)
+        SYSTEM_COLLECTIONURI: $(System.CollectionUri)
+        SYSTEM_TEAMPROJECT: $(System.TeamProject)
